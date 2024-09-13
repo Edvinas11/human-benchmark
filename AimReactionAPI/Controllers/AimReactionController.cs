ed88d6cb0cb6a92a6e54b6041b517e13f07abd2a
Edvinas Burba <edvinas.burba@mif.stud.vu.lt>
2024-09-13 17:35:55 +0300

    Initial setup

---
+++ AimReactionAPI/Controllers/AimReactionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AimReactionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AimReactionController : ControllerBase
+    {
+        private readonly ILogger<AimReactionController> _logger;
+
+        public AimReactionController(ILogger<AimReactionController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetAimReaction")]
+        public string Get()
+        {
+            return "Healthy";
+        }
+    }
+}
