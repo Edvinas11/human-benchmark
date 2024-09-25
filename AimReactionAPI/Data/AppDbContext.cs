70f4123bcf12ca3085e50c22459df68a362099cd
Labžentis Eimantas <eimantas.labzentis@mif.stud.vu.lt>
2024-09-25 09:58:54 +0300

    added leadearboard

--- AimReactionAPI/Data/AppDbContext.cs
+++ AimReactionAPI/Data/AppDbContext.cs
@@ -10,6 +10,7 @@ namespace AimReactionAPI.Data
         public DbSet<GameConfig> GameConfigs { get; set; }
         public DbSet<Game> Games { get; set; }
         public DbSet<Target> Targets { get; set; }
+        public DbSet<Score> Scores { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -27,6 +28,15 @@ namespace AimReactionAPI.Data
                         .WithOne()
                         .HasForeignKey(t => t.GameId)
                         .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Score>().HasData(
+                new Score(1, 100, DateTime.UtcNow, GameType.MovingTargets),
+                new Score(2, 150, DateTime.UtcNow, GameType.ReflexTest),
+                new Score(3, 130, DateTime.UtcNow, GameType.CustomChallenge),
+                new Score(4, 175, DateTime.UtcNow, GameType.MovingTargets)
+            );
+
+
         }
     }
 }
---
+++ AimReactionAPI/Migrations/20240925065654_AddScoresTable.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AimReactionAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddScoresTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Scores",
+                keyColumn: "ScoreId",
+                keyValue: 1,
+                column: "Timestamp",
+                value: new DateTime(2024, 9, 25, 6, 56, 54, 426, DateTimeKind.Utc).AddTicks(5430));
+
+            migrationBuilder.UpdateData(
+                table: "Scores",
+                keyColumn: "ScoreId",
+                keyValue: 2,
+                column: "Timestamp",
+                value: new DateTime(2024, 9, 25, 6, 56, 54, 426, DateTimeKind.Utc).AddTicks(5430));
+
+            migrationBuilder.InsertData(
+                table: "Scores",
+                columns: new[] { "ScoreId", "GameType", "Timestamp", "Value" },
+                values: new object[,]
+                {
+                    { 3, 2, new DateTime(2024, 9, 25, 6, 56, 54, 426, DateTimeKind.Utc).AddTicks(5430), 130 },
+                    { 4, 0, new DateTime(2024, 9, 25, 6, 56, 54, 426, DateTimeKind.Utc).AddTicks(5430), 175 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Scores",
+                keyColumn: "ScoreId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Scores",
+                keyColumn: "ScoreId",
+                keyValue: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Scores",
+                keyColumn: "ScoreId",
+                keyValue: 1,
+                column: "Timestamp",
+                value: new DateTime(2024, 9, 25, 6, 51, 42, 214, DateTimeKind.Utc).AddTicks(3290));
+
+            migrationBuilder.UpdateData(
+                table: "Scores",
+                keyColumn: "ScoreId",
+                keyValue: 2,
+                column: "Timestamp",
+                value: new DateTime(2024, 9, 25, 6, 51, 42, 214, DateTimeKind.Utc).AddTicks(3300));
+        }
+    }
+}
