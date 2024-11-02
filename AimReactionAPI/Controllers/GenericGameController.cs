ac07defbc5a61e7442135cb80a9ed2ff66b7ac6d
Edvinas Burba <edvinas.burba@mif.stud.vu.lt>
2024-11-02 20:02:57 +0200

    Add generic class

---
+++ AimReactionAPI/Controllers/GenericGameController.cs
@@ -0,0 +1,32 @@
+﻿using AimReactionAPI.Models;
+using AimReactionAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AimReactionAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GenericGameController : ControllerBase
+{
+    private readonly GameSessionHandler<GameType> _gameSessionHandler;
+
+    public GenericGameController(GameSessionHandler<GameType> gameSessionHandler)
+    {
+        _gameSessionHandler = gameSessionHandler;
+    }
+
+    [HttpPost("{userId}/start/{gameType}")]
+    public async Task<IActionResult> StartGameSession(int userId, GameType gameType)
+    {
+        var session = await _gameSessionHandler.StartSessionAsync(userId, gameType);
+        return Ok(session);
+    }
+
+    [HttpPost("end/{sessionId}")]
+    public async Task<IActionResult> EndGameSession(int sessionId)
+    {
+        var duration = await _gameSessionHandler.EndSessionAsync(sessionId);
+        return Ok(duration);
+    }
+}
