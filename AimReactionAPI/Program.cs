363704a84c8c3347ff6adc92591c369c8130212a
Edvinas Burba <edvinas.burba@mif.stud.vu.lt>
2024-12-14 22:41:01 +0200

    aaam

--- AimReactionAPI/Program.cs
+++ AimReactionAPI/Program.cs
@@ -4,21 +4,18 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var frontendUrl = builder.Configuration["Frontend:Url"];
-if (string.IsNullOrWhiteSpace(frontendUrl))
-{
-    throw new ArgumentNullException("Frontend:Url", "Frontend URL is not configured. Please set 'Frontend:Url' in appsettings.json or environment variables.");
-}
+builder.Services
+    .AddCors(options =>
+    {
+        options.AddPolicy("AllowFrontend",
+        b =>
+        {
+            b.WithOrigins(builder.Configuration["Frontend:Url"]!)
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend",
-    b => {
-        b.WithOrigins(frontendUrl)
-         .AllowAnyMethod()
-         .AllowAnyHeader();
     });
-});
 
 // Add services to the container.
 
