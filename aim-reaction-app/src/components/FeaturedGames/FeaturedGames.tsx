f2aec435c6480208dfaba95c282f83dafaa880a9
Edvinas Burba <edvinas.burba@mif.stud.vu.lt>
2024-12-14 13:19:29 +0200

    remove cto

--- aim-reaction-app/src/components/FeaturedGames/FeaturedGames.tsx
+++ aim-reaction-app/src/components/FeaturedGames/FeaturedGames.tsx
@@ -82,11 +82,6 @@ const FeaturedGames = () => {
           </div>
         )}
       </div>
-
-      <div className="Reaction">
-        <h2>Play Reaction Test</h2>
-        <Button label="Play Now" variant="primary" onClick={() => { navigate('/reaction-test') }}/>
-      </div>
     </section>
   );
 };
